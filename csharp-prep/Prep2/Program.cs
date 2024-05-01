9efa1f2809be4c5a52215daa6b682b73c2f78656
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2024-05-01 15:08:03 -0600

    Prep 1 and Prep 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,96 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter grade percentage: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter;
+
+        /*
+        if (grade >= 90)
+        {
+            Console.WriteLine($"{grade} earns an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine($"{grade} earns a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine($"{grade} earns a C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine($"{grade} earns a D");
+        }
+        else
+        {
+            Console.WriteLine($"{grade} earns an F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"You passed!");
+        }
+        else
+        {
+            Console.WriteLine($"You did not pass. Better luck next time.");
+        }
+        */
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string plusMinus;
+        int lastDigit = grade % 10;
+        if (lastDigit >= 7)
+        {
+            plusMinus = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            plusMinus = "-";
+        }
+        else 
+        {
+            plusMinus = "";
+        }
+
+
+        if (grade >= 93 || letter == "F")
+        {
+            plusMinus = "";
+        }
+
+        Console.WriteLine($"Your grade: {letter}{plusMinus} ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"You passed!");
+        }
+        else
+        {
+            Console.WriteLine($"You did not pass. Better luck next time.");
+        }
+
     }
 }
