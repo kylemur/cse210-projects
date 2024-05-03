9885b09d884590ceffdb184afbf8dc7ee1b9ccbf
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2024-05-03 16:31:32 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+    
+        // Console.Write("What is the magic number? ");
+        // string numberStr = Console.ReadLine();
+        // int number = int.Parse(numberStr);
+
+        string again = "yes";
+        while (again == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+            int attempt = 0;
+
+            string playing = "yes";
+            while (playing == "yes")
+            {
+                Console.Write("What is your guess? ");
+                string guessStr = Console.ReadLine();
+                int guess = int.Parse(guessStr);
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                    playing = "no";
+                }
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Guess lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Guess higher");
+                }
+
+                attempt++;
+            }
+
+            Console.WriteLine($"You took {attempt} attempts. ");
+
+            Console.Write("Do you want to play again? ");
+            again = Console.ReadLine();
+        }
+        Console.WriteLine("Goodbye");
     }
 }
