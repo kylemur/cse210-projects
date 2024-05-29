b705872ce819ac707ce1bb55d816dad53070f0e8
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2024-05-29 10:02:26 -0600

    Learning 3 completed

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,40 +5,46 @@ public class Fraction
 
     public Fraction()
     {
-
+        _top = 1;
+        _bottom = 1;
     }
     public Fraction(int wholeNumber)
     {
-
+        _top = wholeNumber;
+        _bottom = 1;
     }
     public Fraction(int top, int bottom)
     {
-
+        _top = top;
+        _bottom = bottom;
     }
 
-    public void GetTop()
-    {
-
-    }
-    public void SetTop(int top)
-    {
-
-    }
-    public void GetBottom()
-    {
-
-    }
-    public void SetBottom()
-    {
-
-    }
+            // This part is unnecessary. 
+    // public void GetTop()
+    // {
+    //     Console.WriteLine(_top);
+    // }
+    // public void SetTop(int top)
+    // {
+    //     _top = top;
+    // }
+    // public void GetBottom()
+    // {
+    //     Console.WriteLine(_bottom);
+    // }
+    // public void SetBottom(int bottom)
+    // {
+    //     _bottom = bottom;
+    // }
 
     public string GetFractionString()
     {
-        return "...";
+        return $"{_top}/{_bottom}";
     }
     public double GetDecimalValue()
     {
-        return 1.23;
+        double top = _top;
+        double bottom = _bottom;
+        return top / bottom;
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+                // User interactive
+        // Console.Write("What is the top number? ");
+        // string topString = Console.ReadLine();
+        // int topInt = int.Parse(topString);
+
+        // Console.Write("What is the bottom number? ");
+        // string bottomString = Console.ReadLine();
+        // int bottomInt = int.Parse(bottomString);
+
+        // Fraction fraction1 = new Fraction(topInt, bottomInt);
+        // Console.WriteLine(fraction1.GetFractionString());
+        // Console.WriteLine(fraction1.GetDecimalValue());
+        
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
