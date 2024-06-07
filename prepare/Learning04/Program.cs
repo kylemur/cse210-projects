b4ea51510a17ebd70e239481f626da054878afc3
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2024-06-07 13:47:58 -0600

    Inheritance Prepare complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new("Mike Scott", "Advanced Writing\n");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new("John Bytheway", "Algebra 2", "7.3", "8-19\n");
+        string summary = mathAssignment1.GetSummary();
+        string homeworkList = mathAssignment1.GetHomeworkList();
+        Console.WriteLine(summary);
+        Console.WriteLine(homeworkList);
+    
+        WritingAssignment writingAssignment1 = new("Mary Waters", "European History", "Causes of WWII");
+        Console.WriteLine(writingAssignment1.GetSummary() + "\n" + writingAssignment1.GetWritingInformation());
     }
 }
