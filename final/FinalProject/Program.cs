db94fab29f2ec3394a77366dee5a5f3b53bb5dcd
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2024-07-14 01:55:12 -0600

    added menu to Program

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,8 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        // Console.WriteLine("Hello FinalProject World!");
 
-        
+        string option = "";
+        while(option != "8")
+        {
+            Console.WriteLine();
+            Console.WriteLine("What type of team/player are you recording?");
+            Console.WriteLine(" 1. Baseball");
+            Console.WriteLine(" 2. Basketball");
+            Console.WriteLine(" 3. Football");
+            Console.WriteLine(" 4. Golf");
+            Console.WriteLine(" 5. Hockey");
+            Console.WriteLine(" 6. Soccer");
+            Console.WriteLine(" 7. Wrestling");
+            Console.WriteLine(" 8. Exit");
+            Console.Write("Select a choice from the menu: ");
+            option = Console.ReadLine();
+            Console.WriteLine(" "); // makes a clear line for formatting/organization
+
+
+            if (option == "1") // Baseball
+            {
+
+            }
+            else if (option == "2")
+            {
+
+            }
+            else if (option == "3")
+            {
+
+            }
+            else if (option == "4")
+            {
+
+            }
+            else if (option == "5")
+            {
+
+            }
+            else if (option == "6")
+            {
+
+            }
+            else if (option == "7")
+            {
+
+            }
+            else if (option == "8")
+            {
+                Console.WriteLine("Goodbye.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry.");
+
+            }
+
+        }
     }
 }
